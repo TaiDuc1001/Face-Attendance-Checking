96b2e6bf434784c8fbb88db3de161fb48b34df33
taiduc1001 <phantaiduc2005@gmail.com>
2024-04-15 16:38:15 +0700

    Feat: Sync from firebase to csv

--- upload_data_to_database.py
+++ upload_data_to_database.py
@@ -14,17 +14,38 @@ firebase_admin.initialize_app(
 
 ref = db.reference("Students")
 
-# Open the CSV file
-with open('info.csv', mode='r') as csv_file:
-    data = {}
+def update_csv_from_firebase():
+    firebase_data = ref.get()
+    with open('info.csv', mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        csv_data = {row['id']: row for row in csv_reader}
+        
+        for key, value in firebase_data.items():
+            if key in csv_data:
+                csv_data[key].update(value)
+            else:
+                csv_data[key] = value
+                
+    with open('info.csv', mode='w', newline='') as csv_file:
+        fieldnames = ['id', 'name', 'major', 'total_attendance']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in csv_data.values():
+            writer.writerow(row)
 
-    csv_reader = csv.DictReader(csv_file)
-    for row in csv_reader:
-        row['starting_year'] = int(row['starting_year'])
-        row['total_attendance'] = int(row['total_attendance'])
-        row['year'] = int(row['year'])
-        data[row['id']] = row
+#### IMPORTANT #####
+FIREBASE_TO_CSV = True # Change to False if there is new student
+if FIREBASE_TO_CSV:
+    update_csv_from_firebase()
+else:
+    with open('info.csv', mode='r') as csv_file:
+        data = {}
+
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            row['total_attendance'] = int(row['total_attendance'])
+            data[row['id']] = row
 
 
-for key, value in data.items():
-	ref.child(key).set(value)
+    for key, value in data.items():
+        ref.child(key).set(value)
