2cc152466a656c762081bda5a81e74916fd84e68
taiduc1001 <phantaiduc2005@gmail.com>
2024-04-22 21:34:06 +0700

    Feat: upload images to firebase storage

--- encoder.py
+++ encoder.py
@@ -1,4 +1,7 @@
 from facenet_pytorch import MTCNN, InceptionResnetV1
+import firebase_admin
+from firebase_admin import credentials, storage
+from dotenv import load_dotenv 
 import torch
 from torchvision import datasets
 from torch.utils.data import DataLoader
@@ -7,13 +10,29 @@ from config import DATASET_PATH, DATA_PATH, STAGE_PATH
 import argparse
 import shutil
 import os
+import json
 
+# === ArgParse ===
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--isNew', action='store_true', help='Whether to process new data')
 args = parser.parse_args()
 
 isNew = args.isNew
 
+# === Load .env ===
+load_dotenv()
+service_json = os.getenv("FIREBASE_KEY")
+service_key = json.loads(service_json)
+cred = credentials.Certificate(service_key)
+firebase_admin.initialize_app(
+	cred,
+	{
+		"databaseURL": "https://face-identification-real-time-default-rtdb.firebaseio.com/",
+		"storageBucket": "face-identification-real-time.appspot.com"
+	}
+)
+
+# === MTCNN and RESNET ===
 mtcnn = MTCNN()
 resnet = InceptionResnetV1(pretrained="vggface2").eval()
 
@@ -26,7 +45,7 @@ else:
     old_name_list = []
 
 @timing
-def get_embed_data(isNew=True):
+def get_embed_data_and_upload_images(isNew):
     dataset_path = STAGE_PATH if isNew else DATASET_PATH
     dataset = datasets.ImageFolder(dataset_path)
     idx_to_class = {i: c for c, i in dataset.class_to_idx.items()}
@@ -35,6 +54,13 @@ def get_embed_data(isNew=True):
         return x[0]
 
     loader = DataLoader(dataset, collate_fn=collate_func)
+    path = []
+    for subfolder in os.listdir(dataset_path):
+        image_path = f"{dataset_path}/{subfolder}"
+        path.append(image_path)
+
+    i=0
+    num_images = len(os.listdir(path[0]))
     embeddings_dict = {}
     for img, idx in loader:
         face, prob = mtcnn(img, return_prob=True)
@@ -44,6 +70,16 @@ def get_embed_data(isNew=True):
                 embeddings_dict[idx] = [embeddings]
             else:
                 embeddings_dict[idx].append(embeddings)
+        
+        if i == num_images:
+            i = 0
+        i += 1
+        filename = f"{path[idx]}/{i}.jpg"
+        bucket = storage.bucket()
+        blob = bucket.blob(filename)
+        blob.upload_from_filename(filename)
+        print(f"Uploaded {filename}")
+        num_images = len(os.listdir(path[idx]))
 
     embedding_list = []
     name_list = []
@@ -54,7 +90,7 @@ def get_embed_data(isNew=True):
 
     return embedding_list, name_list
     
-embedding_list, name_list = get_embed_data(isNew)
+embedding_list, name_list = get_embed_data_and_upload_images(isNew)
 if isNew:
     old_embedding_list.extend(embedding_list)
     old_name_list.extend(name_list)
