358f632a22782fbe5e8a420aa6c13387f8619072
taiduc1001 <phantaiduc2005@gmail.com>
2024-04-22 17:35:12 +0700

    Add test path and threshold

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+# Paths
 STAGE_PATH = "stage"
 DATASET_PATH = "fake-dataset"
+EXTRACTED_FACES_PATH = "extracted-faces"
 DATA_PATH = "data.pt"
+TEST_IMAGES_PATH = "test"
+
+# Constants
+THRESHOLD = 0.7
