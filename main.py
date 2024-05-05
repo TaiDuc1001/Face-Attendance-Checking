2f21fa041f363fcddb9d8b751da903e32bc8292b
taiduc1001 <phantaiduc2005@gmail.com>
2024-05-05 12:20:29 +0700

    Feat: add vgg16 to compare faces, but raised bat results

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from facenet_pytorch import MTCNN, InceptionResnetV1
 import torch
+from facenet_pytorch import MTCNN, InceptionResnetV1
+from torchvision.models import vgg16
 import torch.nn.functional as F
+
 from PIL import Image
 from helper import timing
-from config import DATA_PATH, EXTRACTED_FACES_PATH, THRESHOLD
 import os
-import numpy as np
+from config import RESNET_DATA_PATH, VGG_DATA_PATH, EXTRACTED_FACES_PATH, THRESHOLD, VGG_WEIGHTS_PATH
 
-# Init mtcnn and resnet model
+# Init mtcnn, resnet and vgg16 model
 mtcnn = MTCNN()
 resnet = InceptionResnetV1(pretrained="vggface2").eval()
+vgg_model = vgg16(pretrained=False)
+vgg_model.load_state_dict(torch.load(VGG_WEIGHTS_PATH))
+vgg_model.eval()
 
 def calculate_score(predictions, targets, alpha):
     cosine_similarity = F.cosine_similarity(predictions, targets).item()
@@ -18,14 +22,20 @@ def calculate_score(predictions, targets, alpha):
     return score
 
 @timing
-def face_match(image_path, data_path):
+def face_match(image_path, data_path, model):
     img = Image.open(image_path)
     face = mtcnn(img)
+    model = resnet if model == "resnet" else vgg_model
+
     if face is not None:
-        embeddings = resnet(face.unsqueeze(0)).detach()
+        face = face.unsqueeze(0)
+        embeddings = model(face).detach()
+
+        # === Load data ===
         saved_data = torch.load(data_path)
         embedding_list = saved_data[0]
         name_list = saved_data[1]
+
         score_list = []
         for old_embeddings in embedding_list:
             score = calculate_score(embeddings, old_embeddings, alpha=0.8)
@@ -44,9 +54,10 @@ def face_match(image_path, data_path):
 image_files = os.listdir(EXTRACTED_FACES_PATH)
 for image_file in image_files:
     image_path = os.path.join(EXTRACTED_FACES_PATH, image_file)
-    person, score, _ = face_match(image_path=image_path, data_path=DATA_PATH)
-    if person is not None:
-        print(f"Image {image_file} --- Person: {person} --- Similarity: {score:.4f}\n")
+    r_person, r_score, _ = face_match(image_path=image_path, data_path=RESNET_DATA_PATH, model="resnet")
+    v_person, v_score, _ = face_match(image_path=image_path, data_path=VGG_DATA_PATH, model="vgg")
+    if r_person is not None or v_person is not None:
+        print(f"{image_file} --- R_Person: {r_person} --- V_Person: {v_person} --- R_Similarity: {r_score:.4f} --- V_Similarity: {v_score:.4f}\n")
     else:
-        print(f"Image {image_file}: no face detected.\n")
+        print(f"{image_file}: no face detected.\n")
     
