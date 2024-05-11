4099eb81715b9141933779e6f6687f9e48031611
taiduc1001 <phantaiduc2005@gmail.com>
2024-05-11 08:23:37 +0700

    Mod: merge 2 embedding function to achieve generalization

--- encoder.py
+++ encoder.py
@@ -56,57 +56,77 @@ def load_available_data(data_path, isNew):
 @timing
 def get_embed_data(isNew, model_name):
     dataset_path = STAGE_PATH if isNew else DATASET_PATH
-    dataset = datasets.ImageFolder(dataset_path)
-    idx_to_class = {i: c for c, i in dataset.class_to_idx.items()}
-    model = model_dict[model_name]["model"]
-
-    def collate_func(batch):
-        faces = []
-        indices = []
-        threads = []
-
-        def process_image(img, idx):
-            face, prob = mtcnn(img, return_prob=True)
-            if face is not None and prob >= 0.9:
-                faces.append(face)
-                indices.append(idx)
-
-        for img, idx in batch:
-            thread = Thread(target=process_image, args=(img, idx))
-            threads.append(thread)
-            thread.start()
-
-        for thread in threads:
-            thread.join()
-        return faces, indices
-
-    loader = DataLoader(dataset, batch_size=BATCH_SIZE, collate_fn=collate_func)
-    embeddings_dict = {}
-
-    for batch_faces, batch_indices in loader:
-        # Move faces to device before processing
-        faces = [face.to(device) for face in batch_faces]
-        with torch.no_grad():
-            faces = [face.unsqueeze(0).to(device) for face in batch_faces]
-            if model_name == "resnet":
-                embeddings = resnet(torch.cat(faces)).cpu()
-                
-        for idx, emb in zip(batch_indices, embeddings):
-            if idx not in embeddings_dict:
-                embeddings_dict[idx] = [emb]
-            else:
-                embeddings_dict[idx].append(emb)
-
     embedding_list = []
     name_list = []
-    for idx, embeddings_list in embeddings_dict.items():
-        avg_embedding = torch.stack(embeddings_list).mean(dim=0)
-        embedding_list.append(avg_embedding)
-        name_list.append(idx_to_class[idx])
+    print("Embedding data...")
+    if model_name != "vgg-face":
+        dataset = datasets.ImageFolder(dataset_path)
+        idx_to_class = {i: c for c, i in dataset.class_to_idx.items()}
+        model = model_dict[model_name]["model"]
 
-    return embedding_list, name_list
+        def collate_func(batch):
+            faces = []
+            indices = []
+            threads = []
 
-@timing
+            def process_image(img, idx):
+                face, prob = mtcnn(img, return_prob=True)
+                if face is not None and prob >= 0.9:
+                    faces.append(face)
+                    indices.append(idx)
+
+            for img, idx in batch:
+                thread = Thread(target=process_image, args=(img, idx))
+                threads.append(thread)
+                thread.start()
+
+            for thread in threads:
+                thread.join()
+            return faces, indices        
+
+        loader = DataLoader(dataset, batch_size=BATCH_SIZE, collate_fn=collate_func)
+        embeddings_dict = {}
+
+        for batch_faces, batch_indices in loader:
+            faces = [face.to(device) for face in batch_faces]
+            with torch.no_grad():
+                faces = [face.unsqueeze(0).to(device) for face in batch_faces]
+                if model_name == "resnet":
+                    embeddings = resnet(torch.cat(faces)).cpu()
+                    
+            for idx, emb in zip(batch_indices, embeddings):
+                if idx not in embeddings_dict:
+                    embeddings_dict[idx] = [emb]
+                else:
+                    embeddings_dict[idx].append(emb)
+
+
+        for idx, embeddings_list in embeddings_dict.items():
+            avg_embedding = torch.stack(embeddings_list).mean(dim=0)
+            embedding_list.append(avg_embedding)
+            name_list.append(idx_to_class[idx])
+
+        return embedding_list, name_list
+
+    else:
+        embeddings = {}
+        executor = ThreadPoolExecutor(max_workers=WORKERS)
+        for person_folder in os.listdir(dataset_path):
+            person_embeddings = []
+            person_images_folder = os.path.join(dataset_path, person_folder)
+            if os.path.isdir(person_images_folder):
+                image_paths = [os.path.join(person_images_folder, image_file) for image_file in os.listdir(person_images_folder)]
+                futures = [executor.submit(encode_image, image_path) for image_path in image_paths]
+                results = [future.result() for future in futures]
+                for result in results:
+                    if result is not None:
+                        person_embeddings.append(result)
+                if person_embeddings:
+                    person_embeddings = np.array(person_embeddings)
+                    average_embedding = np.mean(person_embeddings, axis=0)
+                    embeddings[person_folder] = average_embedding
+        return list(embeddings.values()), list(embeddings.keys())
+
 def encode_image(image_path):
     try:
         embedding = DeepFace.represent(image_path, model_name='VGG-Face', enforce_detection=False)[0]["embedding"]
@@ -116,46 +136,14 @@ def encode_image(image_path):
         return None
 
 @timing
-def get_embed_data_for_vggface(isNew, model_name):
-    images_folder = STAGE_PATH if isNew else DATASET_PATH
-    embeddings = {}
-    executor = ThreadPoolExecutor(max_workers=WORKERS)
-
-    for person_folder in os.listdir(images_folder):
-        person_embeddings = []
-        person_images_folder = os.path.join(images_folder, person_folder)
-
-        if os.path.isdir(person_images_folder):
-            image_paths = [os.path.join(person_images_folder, image_file) for image_file in os.listdir(person_images_folder)]
-            futures = [executor.submit(encode_image, image_path) for image_path in image_paths]
-            results = [future.result() for future in futures]
-
-            for result in results:
-                if result is not None:
-                    person_embeddings.append(result)
-
-            if person_embeddings:
-                person_embeddings = np.array(person_embeddings)
-                average_embedding = np.mean(person_embeddings, axis=0)
-                embeddings[person_folder] = average_embedding
-
-    return embeddings
-    
-@timing
 def save(model_name, data_path, isNew=isNew):
-    if model_name == "vgg-face":
-        data = get_embed_data_for_vggface(isNew, model_name)
-
-    else:
-        old_embedding_list, old_name_list = load_available_data(data_path, isNew)
-        embedding_list, name_list = get_embed_data(isNew, model_name)
-
-        if isNew:
-            old_embedding_list.extend(embedding_list)
-            old_name_list.extend(name_list)
-            embedding_list, name_list = old_embedding_list, old_name_list
-            
-        data = [embedding_list, name_list]
+    old_embedding_list, old_name_list = load_available_data(data_path, isNew)
+    embedding_list, name_list = get_embed_data(isNew, model_name)
+    if isNew:
+        old_embedding_list.extend(embedding_list)
+        old_name_list.extend(name_list)
+        embedding_list, name_list = old_embedding_list, old_name_list
+    data = [embedding_list, name_list]
     print(f"Save {data_path}.")
     torch.save(data, data_path)
 
@@ -165,7 +153,6 @@ def moving():
     f"""
     Move folders from {STAGE_PATH} to {DATASET_PATH} after embedding new students in {STAGE_PATH}
     """
-
     new_folders = os.listdir(STAGE_PATH)
     new_folders_path = [os.path.join(STAGE_PATH, folder) for folder in new_folders]
     destination_path = DATASET_PATH
