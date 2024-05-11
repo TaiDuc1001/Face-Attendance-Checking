b49dbc23cef25860e3251f9fabfc6050059ce29d
taiduc1001 <phantaiduc2005@gmail.com>
2024-05-11 22:04:55 +0700

    Mod: restore to default option

--- helper.py
+++ helper.py
@@ -4,10 +4,4 @@ def timing(func):
         start_time = time.time()
         result = func(*args, **kwargs)
         end_time = time.time()
-        try:
-            message = f"Executed for {func.__name__} with {model_name}: {(end_time-start_time):.4f} seconds."
-        except:
-            message = f"Executed for {func.__name__}: {(end_time-start_time):.4f} seconds."
-        print(message)
-        return result
-    return wrapper
+        print(f"Executed for {func.__name__}: {(end_time-start_time):.4f} seconds.")
