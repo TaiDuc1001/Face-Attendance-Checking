23641f2b6b918acd173008713af537e252c7f7ba
taiduc1001 <phantaiduc2005@gmail.com>
2024-05-11 08:25:34 +0700

    Mod: add workers, data path of vgg-face and batch size

--- config.py
+++ config.py
@@ -2,10 +2,11 @@
 STAGE_PATH = "stage"
 DATASET_PATH = "database"
 EXTRACTED_FACES_PATH = "extracted-faces"
-RESNET_DATA_PATH = "resnet-data.pt"
-VGG_DATA_PATH = "vgg16-data.pt"
 TEST_IMAGES_PATH = "test"
-VGG_WEIGHTS_PATH = "models/vgg16-397923af.pth"
+RESNET_DATA_PATH = "resnet-data.pt"
+VGG_FACE_DATA_PATH = "vgg-face-data.pt"
 
 # Constants
 THRESHOLD = 0.65
+BATCH_SIZE = 16
+WORKERS = 1
