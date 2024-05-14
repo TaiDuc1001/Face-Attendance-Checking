ccbc132256f4caa83922834d837348b1913c139a
taiduc1001 <phantaiduc2005@gmail.com>
2024-05-14 15:53:28 +0700

    Mod: reorganize class

--- main.py
+++ main.py
@@ -20,6 +20,12 @@ class ImageInfo:
         l2_distance = F.pairwise_distance(predictions, targets).item()
         score = alpha * cosine_similarity + (1 - alpha) * l2_distance
         return score
+    
+    def load_available_data(self, data_path):
+        data = torch.load(data_path)
+        embedding_list = data[0]
+        name_list = data[1]
+        return embedding_list, name_list
 
     def face_match(self, image_path, data_path, model_name):
         model = self.model_dict[model_name]["model"]
@@ -30,27 +36,25 @@ class ImageInfo:
                 face = face.unsqueeze(0)
                 embeddings = model(face).detach()
             else:
-                return None, None, None
+                return None, None
 
         else:
             embeddings = [model(image_path, model_name='VGG-Face', enforce_detection=False)[0]["embedding"]]
 
         # === Load data ===
-        saved_data = torch.load(data_path)
-        embedding_list = saved_data[0]
-        name_list = saved_data[1]
+        embedding_list, name_list = self.load_available_data(data_path)
 
         score_list = []
         for old_embeddings in embedding_list:
-            score = self.calculate_score(embeddings, old_embeddings, alpha=0.8)
+            score = self.calculate_score(embeddings, old_embeddings, alpha=model_dict[model_name]["alpha"])
             score_list.append(score)
             
-        idx_min = score_list.index(max(score_list))
+        idx_max = score_list.index(max(score_list))
 
-        if idx_min is not None:
-            return name_list[idx_min], score_list[idx_min], score_list
+        if idx_max is not None:
+            return name_list[idx_max], score_list[idx_max]
         else:
-            return None, None, None
+            return None, None
 
     def analyze_images(self):
         image_files = os.listdir(self.extracted_faces_path)
@@ -60,7 +64,7 @@ class ImageInfo:
             image_path = os.path.join(self.extracted_faces_path, image_file)
             total_score = 0
             for model_name in self.model_dict:
-                person, score, _ = self.face_match(image_path=image_path, data_path=self.model_dict[model_name]["data_path"], model_name=model_name)
+                person, score = self.face_match(image_path=image_path, data_path=self.model_dict[model_name]["data_path"], model_name=model_name)
                 if score is not None:
                     total_score += score * self.model_dict[model_name]["gamma"]
                 if image_file not in image_info:
@@ -74,11 +78,17 @@ class ImageInfo:
                 image_info[image_file][model_name] = a
             self.voting[image_file] = total_score
         return image_info
+
+    def format_result(self, image, person, score, options=[]):
+        print(f"Image: {image}")
+        if score > THRESHOLD:
+            print(f"Person: {person} --- Score: {score}")
+        else:
+            print(f"Person: Unknown --- Score: {score}")
     
-    def print_result(self):
+    def print_result(self, options=[]):
         voting = self.analyze_images()
         for image, models in voting.items():
-            print(f"Image: {image}")
             persons_for_each_image = [data["person"] for data in models.values()]
             isSame = all(person == persons_for_each_image[0] for person in persons_for_each_image)
             if isSame:
@@ -88,11 +98,11 @@ class ImageInfo:
                 max_score_model = max(models.values(), key=lambda x: x['score'])
                 person = max_score_model['person']
                 score = max_score_model['score']
+            
+            self.format_result(image=image, person=person, score=score, options=options)
 
-            if score > THRESHOLD:
-                print(f"Person: {person} --- Score: {score}")
-            else:
-                print(f"Person: Unknown --- Score: {score}")
+        return image, person, score
+
 
 if __name__ == "__main__":
     ImageInfo().print_result()
