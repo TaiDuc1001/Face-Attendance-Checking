9c09aee66e5b6114641ec9b2d980cd625b2ff448
taiduc1001 <phantaiduc2005@gmail.com>
2024-05-17 09:51:24 +0700

    Mod: change data paths

--- config.py
+++ config.py
@@ -3,11 +3,13 @@ STAGE_PATH = "stage"
 DATASET_PATH = "database"
 EXTRACTED_FACES_PATH = "extracted-faces"
 TEST_IMAGES_PATH = "test"
-RESNET_DATA_PATH = "resnet-data.pt"
-VGG_FACE_DATA_PATH = "vgg-face-data.pt"
+RESNET_DATA_PATH = "features/resnet-data.pt"
+VGGFACE_DATA_PATH = "features/vggface-data.pt"
+ARCFACE_DATA_PATH = "features/arcface-data.pt"
+GHOSTFACENET_DATA_PATH = "features/ghostfacenet-data.pt"
 INFO_PATH = "info.json"
 
 # Constants
-THRESHOLD = 0.65
+THRESHOLD = 0
 BATCH_SIZE = 16
 WORKERS = 1
