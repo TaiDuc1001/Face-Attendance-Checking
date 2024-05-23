36f524aeebf7ca4620226e8034bbed70b1cb34f1
taiduc1001 <phantaiduc2005@gmail.com>
2024-05-23 12:17:51 +0700

    Mod: add paths for feature benchmarks

--- config.py
+++ config.py
@@ -1,13 +1,25 @@
-# Paths
+# Folders
 STAGE_PATH = "stage"
-DATASET_PATH = "database"
 EXTRACTED_FACES_PATH = "extracted-faces"
 TEST_IMAGES_PATH = "test"
+
+# Dataset folders
+DATASET_PATH = "database"
+BENCHMARK_PATH = "benchmark"
+
+# Benchmarks features
+BENCHMARK_RESNET_DATA_PATH = "features-benchmark/resnet-data.pt"
+BENCHMARK_VGGFACE_DATA_PATH = "features-benchmark/vggface-data.pt"
+BENCHMARK_ARCFACE_DATA_PATH = "features-benchmark/arcface-data.pt"
+
+# Testings features
 RESNET_DATA_PATH = "features/resnet-data.pt"
 VGGFACE_DATA_PATH = "features/vggface-data.pt"
 ARCFACE_DATA_PATH = "features/arcface-data.pt"
-GHOSTFACENET_DATA_PATH = "features/ghostfacenet-data.pt"
-INFO_PATH = "benchmark.json"
+
+# JSON Paths
+INFO_PATH = "info.json"
+BENCHMARK_INFO_PATH = "benchmark.json"
 
 # Constants
 THRESHOLD = 0
