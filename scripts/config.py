960c44c773c41d6b68ba90e7570a9626a2e340f8
taiduc1001 <phantaiduc2005@gmail.com>
2024-05-31 17:25:43 +0700

    Mod: remove feature data path

--- scripts/config.py
+++ scripts/config.py
@@ -7,11 +7,6 @@ TEST_IMAGES_PATH = "test"
 DATASET_PATH = "database"
 BENCHMARK_PATH = "benchmark"
 
-# Benchmarks features
-BENCHMARK_RESNET_DATA_PATH = "features-benchmark/resnet-data.pt"
-BENCHMARK_VGGFACE_DATA_PATH = "features-benchmark/vggface-data.pt"
-BENCHMARK_ARCFACE_DATA_PATH = "features-benchmark/arcface-data.pt"
-
 # Testings features
 RESNET_DATA_PATH = "features/resnet-data.pt"
 VGGFACE_DATA_PATH = "features/vggface-data.pt"
