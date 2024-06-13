870f1bca7e591e92f5fa92c91b9ea635c1e1f715
taiduc1001 <phantaiduc2005@gmail.com>
2024-06-13 08:28:08 +0700

    Feat: add option to detect with dlib or mtcnn

--- scripts/face_detector.py
+++ scripts/face_detector.py
@@ -9,50 +9,82 @@ import argparse
 import cv2
 import shutil
 import os
+import dlib
+import torch
 from scripts.config import TEST_IMAGES_PATH, EXTRACTED_FACES_PATH
 
-mtcnn = MTCNN()
-resnet = InceptionResnetV1(pretrained='vggface2').eval()
+if __name__ == "__main__":
+    mtcnn = MTCNN()
+    resnet = InceptionResnetV1(pretrained='vggface2').eval()
+    detector = dlib.get_frontal_face_detector()
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-# === ArgParse ===
-parser = argparse.ArgumentParser(description="Extract faces from an image.")
-parser.add_argument("-f", "--file", type=str, help="File name in TEST_IMAGES_PATH folder")
-args = parser.parse_args()
+    # === ArgParse ===
+    parser = argparse.ArgumentParser(description="Extract faces from an image.")
+    parser.add_argument("-f", "--file", type=str, help="File name in TEST_IMAGES_PATH folder")
+    parser.add_argument("-m", "--method", type=str, help="Method to detect faces: mtcnn or dlib", default="mtcnn")
+    args = parser.parse_args()
 
-if args.file:
-    file_name = args.file
-    image_path = os.path.join(TEST_IMAGES_PATH, file_name)
-else:
-    image_path = os.path.join(TEST_IMAGES_PATH, "1.jpg")
+    if args.file:
+        file_name = args.file
+        image_path = os.path.join(TEST_IMAGES_PATH, file_name)
+    else:
+        image_path = os.path.join(TEST_IMAGES_PATH, "1.jpg")
+    # === Read image ===
+    image = cv2.imread(image_path)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    boxes, probs = mtcnn.detect(image_rgb)
+    detections = detector(image_rgb, 1)
 
-# === Read image ===
-image = cv2.imread(image_path)
-image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-boxes, probs = mtcnn.detect(image_rgb)
+    # In case someone else pull this project
+    if not os.path.exists(EXTRACTED_FACES_PATH):
+        os.mkdir(EXTRACTED_FACES_PATH)
 
-# In case someone else pull this project
-if not os.path.exists(EXTRACTED_FACES_PATH):
-    os.mkdir(EXTRACTED_FACES_PATH)
+    shutil.rmtree(EXTRACTED_FACES_PATH, ignore_errors=True)
+    if not os.path.exists(EXTRACTED_FACES_PATH):
+        os.mkdir(EXTRACTED_FACES_PATH)
 
-shutil.rmtree(EXTRACTED_FACES_PATH, ignore_errors=True)
-if not os.path.exists(EXTRACTED_FACES_PATH):
-    os.mkdir(EXTRACTED_FACES_PATH)
+    face_counted = 0
+    choice = args.method
+    if choice == "mtcnn":
+        print("Using MTCNN...")
+        if boxes is not None:
+            for i, (box, prob) in enumerate(zip(boxes, probs)):
+                i += 1
+                if prob >= 0.9:
+                    face_counted += 1
+                    x1, y1, x2, y2 = map(int, box)
+                    x1, y1 = max(x1, 0), max(y1, 0)
+                    x2, y2 = min(x2, image.shape[1]), min(y2, image.shape[0])
+                    face = image[y1:y2, x1:x2]
 
-face_counted = 0
-if boxes is not None:
-    for i, (box, prob) in enumerate(zip(boxes, probs)):
-        i += 1
-        if prob >= 0.9:
-            face_counted += 1
-            x1, y1, x2, y2 = map(int, box)
-            x1, y1 = max(x1, 0), max(y1, 0)
-            x2, y2 = min(x2, image.shape[1]), min(y2, image.shape[0])
+                    cv2.imwrite(os.path.join(EXTRACTED_FACES_PATH, f"{i}.jpg"), face)
+                    cv2.imshow('Extracted Face', face)
+                    key = cv2.waitKey(5000) & 0xFF
+                    if key == ord('q'):
+                        continue
+    elif choice == "dlib":
+        print("Using dlib...")
+        for i, d in enumerate(detections):
+            i += 1
+            x1, y1, x2, y2 = d.left(), d.top(), d.right(), d.bottom()
             face = image[y1:y2, x1:x2]
+            # Resize face to match the input size of the resnet model
+            face = cv2.resize(face, (160, 160))
+            
+            # Convert face to tensor
+            face_tensor = torch.tensor(face, dtype=torch.float32).permute(2, 0, 1).unsqueeze(0).to(device)
+            
+            # Inference using resnet model
+            embeddings = resnet(face_tensor).detach().cpu().numpy()
 
+            # Saving the face
             cv2.imwrite(os.path.join(EXTRACTED_FACES_PATH, f"{i}.jpg"), face)
             cv2.imshow('Extracted Face', face)
             key = cv2.waitKey(5000) & 0xFF
             if key == ord('q'):
                 continue
-print(f"Detected {face_counted} faces. Saved in {EXTRACTED_FACES_PATH}.")
-cv2.destroyAllWindows()
+            face_counted += 1
+
+    print(f"Detected {face_counted} faces. Saved in {EXTRACTED_FACES_PATH}.")
+    cv2.destroyAllWindows()
