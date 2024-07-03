65bbf01cca6919c795bc400a48e9850606cba23c
taiduc1001 <phantaiduc2005@gmail.com>
2024-07-03 16:05:17 +0700

    Chore

--- scripts/config.py
+++ scripts/config.py
@@ -1,22 +1,16 @@
 # Folders
 STAGE_PATH = "stage"
-EXTRACTED_FACES_PATH = "extracted-faces"
-TEST_IMAGES_PATH = "test"
+FACES_PATH = "faces"
 
 # Dataset folders
-DATASET_PATH = "database"
-BENCHMARK_PATH = "benchmark"
+DATABASE_PATH = "database"
+FEATURES_PATH = "features"
 
 # Testings features
 RESNET_DATA_PATH = "features/resnet-data.pt"
 VGGFACE_DATA_PATH = "features/vggface-data.pt"
-ARCFACE_DATA_PATH = "features/arcface-data.pt"
+FACENET512_DATA_PATH = "features/facenet512-data.pt"
+OPENFACE_DATA_PATH = "features/openface-data.pt"
 
 # JSON Paths
-INFO_PATH = "json/info.json"
-BENCHMARK_INFO_PATH = "json/benchmark.json"
-
-# Constants
-THRESHOLD = 0
-BATCH_SIZE = 16
-WORKERS = 1
+META_PATH = "meta"
