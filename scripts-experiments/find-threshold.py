e8e7571b1fec0165c9288745fdaad7fc700db331
taiduc1001 <phantaiduc2005@gmail.com>
2024-07-03 14:50:45 +0700

    Init

---
+++ scripts-experiments/find-threshold.py
@@ -0,0 +1,7 @@
+import sys
+import os
+script_dir = os.path.dirname(os.path.abspath(__file__))
+root_dir = os.path.join(script_dir, '..')
+sys.path.append(root_dir)
+
+from scripts.config import DATABASE_PATH, META_PATH
